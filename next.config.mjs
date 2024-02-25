20c8b1419890cc24262091cff99210d0cf92bd20
Alley Shawwn <cayetuna2000@gmail.com>
2024-02-26 01:37:46 +0800

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      dns: false,
+      tls: false,
+    };
+
+    return config;
+  },
+  env: {
+    JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
+    DB_URL: process.env.DB_URL,
+  },
 };
 
 export default nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "reflect-metadata";
+
+import "@/styles/globals.css";
+import "@/styles//main.styles.css";
+import { ConfigProvider } from "antd";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "abel",
+        },
+      }}
+    >
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <title>VELAYO E-Service</title>
+        <meta
+          name="description"
+          content="A Web System that helps to assist and provide service for walk-in client on different branches"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
