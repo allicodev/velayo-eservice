09f47f17bcc71ee2e31d50a4dc71b922a027ebbc
Alley Shawwn <cayetuna2000@gmail.com>
2024-04-09 00:47:43 +0800

    test 1

--- pages/api/print.js
+++ pages/api/print.js
@@ -13,19 +13,23 @@ async function handler(req, res) {
 
   let printer = new ThermalPrinter({
     type: PrinterTypes.STAR,
-    interface: "bluetooth",
+    interface: "printer:velayo_printer",
   });
 
-  printer.alignCenter();
-  printer.println("Hello world");
+  // printer.alignCenter();
+  // printer.println("Hello world");
+  let a = await printer.isPrinterConnected();
+  console.log(a);
+  printer.print("Hi Love, I love you");
   printer.cut();
 
-  try {
-    let execute = printer.execute();
-    console.log("Print done!");
-  } catch (error) {
-    console.error("Print failed:", error);
-  }
+  // try {
+  //   let execute = await printer.execute();
+  //   console.log(execute);
+  //   console.log("Print done!");
+  // } catch (error) {
+  //   console.error("Print failed:", error);
+  // }
 
   res.json({ code: 200, success: true });
 }
