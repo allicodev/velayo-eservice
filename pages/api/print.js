6dd1fa27cad63674d7133a412137a4179c451d14
Alley Shawwn <cayetuna2000@gmail.com>
2024-04-09 01:46:27 +0800

    test2

--- pages/api/print.js
+++ pages/api/print.js
@@ -1,5 +1,14 @@
-const ThermalPrinter = require("node-thermal-printer").printer;
-const PrinterTypes = require("node-thermal-printer").types;
+// const ThermalPrinter = require("node-thermal-printer").printer;
+// const PrinterTypes = require("node-thermal-printer").types;
+
+var thermal_printer = require("node-thermal-printer");
+var printer = require("printer");
+
+var star = printer.getPrinter("STMicroelectronics_POS80_Printer_USB");
+
+thermal_printer.init({
+  type: "star",
+});
 
 async function handler(req, res) {
   const { method } = req;
@@ -11,17 +20,46 @@ async function handler(req, res) {
       message: "Incorrect Request Method",
     });
 
-  let printer = new ThermalPrinter({
-    type: PrinterTypes.STAR,
-    interface: "printer:velayo_printer",
+  console.log(printer.getPrinters());
+
+  thermal_printer.print("Hello World");
+  thermal_printer.println("Hello World");
+  thermal_printer.println("Before partial cut...");
+  thermal_printer.partialCut();
+  thermal_printer.println("After partial cut...");
+  thermal_printer.cut();
+  thermal_printer.print("Hello World");
+  thermal_printer.println("Hello World");
+  thermal_printer.println("Before partial cut...");
+  thermal_printer.partialCut();
+  thermal_printer.println("After partial cut...");
+  thermal_printer.drawLine();
+
+  console.log(thermal_printer.getBuffer());
+
+  printer.printDirect({
+    data: thermal_printer.getBuffer(),
+    printer: star.name,
+    type: "TEXT",
+    success: function (job_id) {
+      console.log("OK :" + job_id);
+    },
+    error: function (err) {
+      console.error(err);
+    },
   });
 
-  // printer.alignCenter();
-  // printer.println("Hello world");
-  let a = await printer.isPrinterConnected();
-  console.log(a);
-  printer.print("Hi Love, I love you");
-  printer.cut();
+  // let printer = new ThermalPrinter({
+  //   type: PrinterTypes.STAR,
+  //   interface: "printer:velayo_printer",
+  // });
+
+  // // printer.alignCenter();
+  // // printer.println("Hello world");
+  // let a = await printer.isPrinterConnected();
+  // console.log(a);
+  // printer.print("Hi Love, I love you");
+  // printer.cut();
 
   // try {
   //   let execute = await printer.execute();
