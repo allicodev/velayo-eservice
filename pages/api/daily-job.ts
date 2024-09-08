caf090fc3af1a998b07ec9161edadc07d02250e8
Alley Shawwn <cayetuna2000@gmail.com>
2024-09-09 00:29:25 +0800

    fix photo null on cron job (flexTime updates)

--- pages/api/daily-job.ts
+++ pages/api/daily-job.ts
@@ -1,9 +1,9 @@
 import dbConnect from "@/database/dbConnect";
-import Log from "@/database/models/log.schema";
 import {
   queueResetter,
   creditInterestChecker,
   resetBranchInitialBalance,
+  attendancePhotoResetter,
 } from "@/provider/utils";
 import { ExtendedResponse, Response } from "@/types";
 
@@ -24,43 +24,10 @@ async function handler(
       message: "Incorrect Request Method",
     });
 
-  const currentDate: Date = new Date();
-  const pastDate: Date = new Date(
-    currentDate.setDate(currentDate.getDate() - 15)
-  );
-
   await queueResetter();
   await creditInterestChecker();
   await resetBranchInitialBalance();
-
-  return await Log.find(
-    {
-      type: "attendance",
-      $or: [{ timeIn: { $lte: pastDate } }, { timeOut: { $lte: pastDate } }],
-    },
-    { _id: 1 }
-  )
-    .select("_id")
-    .then(async (e) => {
-      await Log.updateMany(
-        { _id: { $in: e.map((_) => _._id.toString()) } },
-        { timeInPhoto: null, timeOutPhoto: null }
-      );
-
-      return res.json({
-        code: 200,
-        success: true,
-        data: e as any,
-      });
-    })
-    .catch((e) => {
-      console.log(e);
-      return res.json({
-        code: 500,
-        success: false,
-        message: "There is an error in the Server.",
-      });
-    });
+  await attendancePhotoResetter(res);
 }
 
 export default handler;
--- provider/utils/index.ts
+++ provider/utils/index.ts
@@ -2,7 +2,8 @@ import Settings from "@/database/models/settings.schema";
 import Branch from "@/database/models/branch.schema";
 import Log from "@/database/models/log.schema";
 import dayjs from "dayjs";
-import balance from "@/pages/api/portal/balance";
+import { NextApiResponse } from "next";
+import { ExtendedResponse, Response } from "@/types";
 
 const queueResetter = async () => {
   // if exist
@@ -126,9 +127,47 @@ const resetBranchInitialBalance = async () => {
   );
 };
 
+const attendancePhotoResetter = async (
+  res: NextApiResponse<ExtendedResponse<Response>>
+) => {
+  const currentDate: Date = new Date();
+  const pastDate: Date = new Date(
+    currentDate.setDate(currentDate.getDate() - 15)
+  );
+
+  return await Log.find(
+    {
+      type: "attendance",
+      createdAt: { $lte: pastDate },
+    },
+    { _id: 1 }
+  )
+    .select("_id")
+    .then(async (e) => {
+      await Log.updateMany(
+        { _id: { $in: e.map((_) => _._id.toString()) } },
+        { $set: { "flexiTime.$[].photo": null } }
+      );
+      return res.json({
+        code: 200,
+        success: true,
+        data: e as any,
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+      return res.json({
+        code: 500,
+        success: false,
+        message: "There is an error in the Server.",
+      });
+    });
+};
+
 export {
   queueResetter,
   creditInterestChecker,
   processCreditPayment,
   resetBranchInitialBalance,
+  attendancePhotoResetter,
 };
